172a15321123ffa21bdfeac99e716fe964ac8fde
alexis-1729 <ibaa59733@gmail.com>
2024-10-28 20:43:23 -0600

    se agrego tailwind

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    console.log('click');
+    // const button = document.getElementById('buttonMenu');
+    // const estado = button.getAttribute('aria-expanded');
+    // if(estado == 'false')
+    // button.setAttribute('aria-expanded', true);
+    // else     button.setAttribute('aria-expanded', false);
+
+
+}
